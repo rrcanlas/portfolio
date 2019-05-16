797da040c0297cc52ff676c0235047addf4c4c78
rose <rrbgc0512@gmail.com>
2019-05-16 16:51:50 -0700

    Updates

---
+++ precache-manifest.d39ab3663049c42a4d69227c567eba86.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6b64d540f1308ee928ad",
+    "url": "/Portfolio/static/js/runtime~main.132fbc8c.js"
+  },
+  {
+    "revision": "ddc08769157201d79965",
+    "url": "/Portfolio/static/js/main.fbda7288.chunk.js"
+  },
+  {
+    "revision": "b49810c1e65931663d84",
+    "url": "/Portfolio/static/js/2.f1a01bde.chunk.js"
+  },
+  {
+    "revision": "ddc08769157201d79965",
+    "url": "/Portfolio/static/css/main.6e43a520.chunk.css"
+  },
+  {
+    "revision": "9995c1884120d4c7f0f4a8328a81f2a5",
+    "url": "/Portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Portfolio/precache-manifest.7d2899be9c613d34a71ae25031ff68fd.js"
+  "/Portfolio/precache-manifest.d39ab3663049c42a4d69227c567eba86.js"
 );
 
 workbox.clientsClaim();
