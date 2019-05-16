5b14eb94e17c3cd94ac024287895ffdf7b7c4365
rose <rrbgc0512@gmail.com>
2019-05-16 11:54:11 -0700

    Updates

---
+++ precache-manifest.018d4ae722680248e448de8651dc79aa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "7302194bc008ea3b21699f14d87d094d",
+    "url": "/Portfolio/static/media/wa-state.7302194b.jpg"
+  },
+  {
+    "revision": "5c62d9e19e62d0e75e4f48615c24dc6c",
+    "url": "/Portfolio/static/media/view.5c62d9e1.jpg"
+  },
+  {
+    "revision": "6b64d540f1308ee928ad",
+    "url": "/Portfolio/static/js/runtime~main.132fbc8c.js"
+  },
+  {
+    "revision": "8c5488f3fecb0840c48f",
+    "url": "/Portfolio/static/js/main.6117121b.chunk.js"
+  },
+  {
+    "revision": "b49810c1e65931663d84",
+    "url": "/Portfolio/static/js/2.f1a01bde.chunk.js"
+  },
+  {
+    "revision": "8c5488f3fecb0840c48f",
+    "url": "/Portfolio/static/css/main.fcd7a28d.chunk.css"
+  },
+  {
+    "revision": "345c1eac6c7d7c85f9b69987685d46cf",
+    "url": "/Portfolio/index.html"
+  }
+];
