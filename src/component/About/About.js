d2d2655a10c02a0617dcb46a158f15bc91824dfd
rose <rrbgc0512@gmail.com>
2019-07-02 23:45:58 -0700

    edit about

--- src/component/About/About.js
+++ src/component/About/About.js
@@ -13,7 +13,7 @@ export default function bio() {
                     Rose Renie Canlas
                 </div>
                 <div className="bio-text">
-                    Experienced Web Developer with a demonstrated history of working in the non-profit organization management industry. Skilled in User Experience (UX), WordPress, User Interface Design, Web Design, and HTML. Alumni of the Grow with Google Developer Scholarship in 2018 and have completed Udacity's Front-End Developer nanodegree.                					
+                    Experienced Web Developer with a demonstrated history of working in the non-profit organization management industry. Skilled in User Experience (UX), WordPress, User Interface Design, Web Design, and HTML. In-depth knowledge of Visual Basic programming. Extensive knowledge in Microsoft Access, MS-SQL and Crystal Report. Alumni of the Grow with Google Developer Scholarship in 2018 and have completed Udacity's Front-End Developer nanodegree.                					
 				</div>
             </div>                   	
         </section>     
